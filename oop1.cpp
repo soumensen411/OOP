cb8a44a4f037f5ed5f650acf1f805fc10f2989ae
Soumen sen <144879914+soumensen411@users.noreply.github.com>
2025-01-16 19:28:54 +0600

    Update oop1.cpp
    First program

--- oop1.cpp
+++ oop1.cpp
@@ -1,3 +1,11 @@
+/*1. Create a class with a function that prints "This is parent class" and its subclass with 
+another function that prints "This is child class". Now, create an object for each class 
+and call 
+a. Function of the parent class by the object of the parent class 
+b. Function of the child class by the object of the child class 
+c. Function of the parent class by the object of the child class. 
+*/    
+
 #include <bits/stdc++.h>
 #define optimize()                \
     ios_base::sync_with_stdio(0); \
